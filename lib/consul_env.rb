6db9ff73d87bcb99cfcd858c8a5558b2c4d23405
Mitch Monsen <mmonsen7@gmail.com>
2016-08-08 10:52:11 -0600

    Adds yaml file as secondary data source option for local/disconnected dev, refactors for dryness

--- lib/consul_env.rb
+++ lib/consul_env.rb
@@ -1,9 +1,23 @@
 require_relative "./consul_env/version"
 require 'httparty'
+require 'yaml'
 
 module ConsulEnv
   def self.load_folder *folder, **opts
-    url = opts.fetch(:consul_url, "http://localhost:8500")
+    if opts[:file_path]
+      config_hash = load_from_yaml opts[:file_path], *folder, **opts
+    elsif opts[:consul_url]
+      config_hash = load_from_consul opts[:consul_url], *folder, **opts
+    else
+      raise ArgumentError, 'No data source supplied! Please supply a consul_url or file_path parameter.'
+    end
+
+    ENV.update(config_hash)
+
+    config_hash
+  end
+
+  def self.load_from_consul url, *folder, **opts
     opts[:token] ||= ENV['CONSUL_TOKEN'] if ENV['CONSUL_TOKEN']
 
     request_options = {
@@ -24,11 +38,51 @@ module ConsulEnv
         }
       end
 
+      config_hash = digest_vars vars_from_consul, opts
+
+      final_hash.merge(config_hash)
+    end
+  end
+
+  def self.load_from_yaml yaml_path, *folder, **opts
+    file = File.read yaml_path
+    vars = YAML.load file
+
+    reducer = -> (hashy, key_arr = [], accum = {}) do
+      return accum.merge!( { key_arr.join('/') => hashy } ) unless hashy.is_a? Hash
+
+      hashy.each_pair do |key, val|
+        reducer.call(val, key_arr + [key], accum)
+      end
+
+      accum
+    end
+
+    consul_vars = reducer.call vars['consul'].select { |k, v| folder.include?(k) }
+
+    key_val_pairs = consul_vars.map do |key, val|
+      {
+        key: key.gsub(/(#{folder.join('|')})\//, ''),
+        value: val
+      }
+    end
+
+    config_hash = digest_vars key_val_pairs, opts
+
+    ENV.update(config_hash)
+
+    config_hash
+  end
+
+  private
+
+    def self.digest_vars variables, opts = nil
+      opts ||= {}
       seed = Hash.new { |h, k| h[k] = Hash.new(&h.default_proc) }
 
       dropped_keys = ['env', *opts.fetch(:drop_prefixes, [])]
 
-      config_hash = vars_from_consul.reduce(seed) do |accum, hashy|
+      variables.reduce(seed) do |accum, hashy|
         consul_key, val = hashy[:key].split('/'), hashy[:value]
         
         consul_key = consul_key - dropped_keys
@@ -38,12 +92,5 @@ module ConsulEnv
         # IE: SCREAMING_SNAKE_CASE
         accum.merge({ consul_key.join("_").upcase => val })
       end
-
-      final_hash.merge(config_hash)
     end
-
-    ENV.update(combined_config_hash)
-
-    combined_config_hash
-  end
 end
--- test/test_consul_env.rb
+++ test/test_consul_env.rb
@@ -11,41 +11,54 @@ class TestConsulEnv < MiniTest::Test
 
   def test_load_folder
     HTTParty.stub :get, ALS_CONSUL_FOLDER do
-      ConsulEnv.load_folder folder
+      result = ConsulEnv.load_folder folder, consul_url: "http://localhost:8500"
 
-      assert_equal 'db_user', ENV['DB_USER']
+      assert_equal 'db_user', result['DB_USER']
     end
   end
 
   def test_load_folder_with_env
     HTTParty.stub :get, ALS_CONSUL_FOLDER do
-      ConsulEnv.load_folder folder
+      result = ConsulEnv.load_folder folder, consul_url: "http://localhost:8500"
 
-      assert_equal 'tomcat_url', ENV['TOMCAT_SERVER']
+      assert_equal 'tomcat_url', result['TOMCAT_SERVER']
     end
   end
 
   def test_load_folder_with_config
     HTTParty.stub :get, ALS_CONSUL_FOLDER do
-      ConsulEnv.load_folder folder, consul_url: "http://localhost:8500"
+      result = ConsulEnv.load_folder folder, consul_url: "http://localhost:8500"
 
-      assert_equal 'tomcat_url', ENV['TOMCAT_SERVER']
+      assert_equal 'tomcat_url', result['TOMCAT_SERVER']
     end
   end
 
   def test_dropped_prefixes
     HTTParty.stub :get, NAV_CONSUL_FOLDER do
-      ConsulEnv.load_folder 'NAV', consul_url: "http://localhost:8500", drop_prefixes: ['common', 'urls']
+      result = ConsulEnv.load_folder 'NAV', consul_url: "http://localhost:8500", drop_prefixes: ['common', 'urls']
 
-      assert_equal 'tomcat_url', ENV['TOMCAT_URL']
+      assert_equal 'tomcat_url', result['TOMCAT_URL']
     end
   end
 
   def test_multiple_folders
     HTTParty.stub :get, [*ALS_CONSUL_FOLDER, *NAV_CONSUL_FOLDER] do
-      ConsulEnv.load_folder 'NAV', 'ALS', consul_url: "http://localhost:8500", drop_prefixes: ['common', 'urls']
+      result = ConsulEnv.load_folder 'NAV', 'ALS', consul_url: "http://localhost:8500", drop_prefixes: ['common', 'urls']
 
-      assert_equal 'tomcat_url', ENV['TOMCAT_URL']
+      assert_equal 'tomcat_url', result['TOMCAT_URL']
     end
+  end
+
+  def test_load_from_yaml
+    yaml_path = "#{`pwd`.chomp}/test/test_vars.yml"
+
+    result = ConsulEnv.load_folder 'NAV', 'ALS', file_path: yaml_path, drop_prefixes: ['common', 'urls']
+    
+    assert_equal 'tomcat_url', result['TOMCAT_URL']
+    assert_equal 'redis_url', result['REDIS_URL']
+  end
+
+  def test_raises_error_without_source_option
+    assert_raises(ArgumentError) { ConsulEnv.load_folder 'NAV' }
   end
 end
